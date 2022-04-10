cad3296541782818bded4daab8eaffe7be459ca6
csumn <csumn13@gmail.com>
2022-04-10 12:28:50 +0530

    Refactor to throw custom exceptions in case of invalid user details

---
+++ UserRegistration/src/main/java/com/bridgelabz/InvalidUserException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+@SuppressWarnings("serial")
+public class InvalidUserException extends Exception {
+
+	ExceptionType type;
+
+	enum ExceptionType {
+		ENTERED_INVALID, ENTERED_NULL
+	}
+	public InvalidUserException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
